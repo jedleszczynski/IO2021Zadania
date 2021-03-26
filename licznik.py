f37f7ca65bd515a2b3627d71870552ff918a6944
jed <jedrzej.leszczynski@gmail.com>
2021-03-26 09:20:15 +0100

    Encloded the program in sprawdz_wyrazy()

--- licznik.py
+++ licznik.py
@@ -1,12 +1,16 @@
-print("Wprowadź 5 wyrazów. Licznik sprawdzi które z nich zacznają się od wielkiej litery")
-counter=0
-for i in range(5):
-    print("Wyraz {}:".format(i+1))
-    try:
-        wyraz=str(input())
-        if(wyraz[0].isupper()):
-            counter+=1
-    except (IOError, ValueError, IndexError):
-        print("Błąd przy wprowadzaniu wyrazu. Wyraz {} pominięty".format(i+1))
-print(counter)        
+def sprawdz_wyrazy():
+    print("Wprowadź 5 wyrazów. Licznik sprawdzi które z nich zacznają się od wielkiej litery")
+    counter=0
+    for i in range(5):
+        print("Wyraz {}:".format(i+1))
+        try:
+            wyraz=str(input())
+            if(wyraz[0].isupper()):
+                counter+=1
+        except (IOError, ValueError, IndexError):
+            print("Błąd przy wprowadzaniu wyrazu. Wyraz {} pominięty".format(i+1))
+    print(counter) 
+sprawdz_wyrazy()     
+
+      
 
